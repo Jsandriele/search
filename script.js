52c9d02834844051fec01ba007d3689bff4b680f
jsandriele <jsandriele@yahoo.com.br>
2023-11-07 00:38:29 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,32 +1,14 @@
-import sdk from '@stackblitz/sdk'
+function tabuada() {
+  const inputNumero = document.getElementById('search-input');
+  const numero = parseInt(inputNumero.value);
 
-sdk.embedProjectId(
-  'elementOrId',
-  'web-platform-yjxvaf',
-  {
-    forceEmbedLayout: true,
-    openFile: 'styles.css',
+  const outputTabuada = document.getElementById('resposta');
+  let tabuadaHTML = '';
+
+  for (let i = 1; i <= 10; i++) {
+    tabuadaHTML += numero + ' X ' + i + ' = ' + numero * i + '<br>';
   }
-);
-
-function tabuada(){
-    const inputNumero = document.getElementById('search-input');
-    const numero = parseInt(inputNumero.value);
-
-    const outputTabuada = document.getElementById('resposta');
-    let tabuada = '';
-
-
-    
-
-    for(let i = 1; i <= 10; i++){
-        tabuada += numero +" X " + i+ " = "+
-        (numero*i)+ "<br>";
-        
-    }
-    tabuada += '';
-    outputTabuada.innerHTML= tabuada();
-
+  tabuada += '';
+  outputTabuada.innerHTML = tabuadaHTML;
 }
-document.getElementById('search-button').addEventListener('click', tabuada());
-console.log(tabuada)
+document.getElementById('button').addEventListener('click', tabuada)
